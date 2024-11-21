255a2875a334108786f462288ad75552eaae67b8
alimudasirpcr <alimudasirpcr@gmail.com>
2024-11-21 10:05:40 +0400

    update

---
+++ application/logs/log-2024-11-20.php
@@ -0,0 +1,128 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2024-11-20 13:39:22 --> Config Class Initialized
+INFO - 2024-11-20 13:39:22 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:39:22 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:39:22 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:39:22 --> URI Class Initialized
+DEBUG - 2024-11-20 13:39:22 --> No URI present. Default controller set.
+INFO - 2024-11-20 13:39:22 --> Router Class Initialized
+INFO - 2024-11-20 13:39:22 --> Output Class Initialized
+INFO - 2024-11-20 13:39:22 --> Security Class Initialized
+DEBUG - 2024-11-20 13:39:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:39:22 --> Input Class Initialized
+INFO - 2024-11-20 13:39:22 --> Language Class Initialized
+INFO - 2024-11-20 13:39:22 --> Loader Class Initialized
+INFO - 2024-11-20 13:39:22 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:39:22 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:39:22 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:39:22 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:39:22 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:39:22 --> Controller Class Initialized
+INFO - 2024-11-20 13:39:22 --> File loaded: application/views\welcome_message.php
+INFO - 2024-11-20 13:39:22 --> Final output sent to browser
+DEBUG - 2024-11-20 13:39:22 --> Total execution time: 0.0236
+INFO - 2024-11-20 13:39:33 --> Config Class Initialized
+INFO - 2024-11-20 13:39:33 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:39:33 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:39:33 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:39:33 --> URI Class Initialized
+DEBUG - 2024-11-20 13:39:33 --> No URI present. Default controller set.
+INFO - 2024-11-20 13:39:33 --> Router Class Initialized
+INFO - 2024-11-20 13:39:33 --> Output Class Initialized
+INFO - 2024-11-20 13:39:33 --> Security Class Initialized
+DEBUG - 2024-11-20 13:39:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:39:33 --> Input Class Initialized
+INFO - 2024-11-20 13:39:33 --> Language Class Initialized
+INFO - 2024-11-20 13:39:33 --> Loader Class Initialized
+INFO - 2024-11-20 13:39:33 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:39:33 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:39:33 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:39:33 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:39:33 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:39:33 --> Controller Class Initialized
+INFO - 2024-11-20 13:39:33 --> Final output sent to browser
+DEBUG - 2024-11-20 13:39:33 --> Total execution time: 0.0211
+INFO - 2024-11-20 13:39:46 --> Config Class Initialized
+INFO - 2024-11-20 13:39:46 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:39:46 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:39:46 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:39:46 --> URI Class Initialized
+DEBUG - 2024-11-20 13:39:46 --> No URI present. Default controller set.
+INFO - 2024-11-20 13:39:46 --> Router Class Initialized
+INFO - 2024-11-20 13:39:46 --> Output Class Initialized
+INFO - 2024-11-20 13:39:46 --> Security Class Initialized
+DEBUG - 2024-11-20 13:39:46 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:39:46 --> Input Class Initialized
+INFO - 2024-11-20 13:39:46 --> Language Class Initialized
+INFO - 2024-11-20 13:39:46 --> Loader Class Initialized
+INFO - 2024-11-20 13:39:46 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:39:46 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:39:46 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:39:46 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:39:46 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:39:46 --> Controller Class Initialized
+INFO - 2024-11-20 13:39:46 --> Final output sent to browser
+DEBUG - 2024-11-20 13:39:46 --> Total execution time: 0.0213
+INFO - 2024-11-20 13:40:54 --> Config Class Initialized
+INFO - 2024-11-20 13:40:54 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:40:54 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:40:54 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:40:54 --> URI Class Initialized
+DEBUG - 2024-11-20 13:40:54 --> No URI present. Default controller set.
+INFO - 2024-11-20 13:40:54 --> Router Class Initialized
+INFO - 2024-11-20 13:40:54 --> Output Class Initialized
+INFO - 2024-11-20 13:40:54 --> Security Class Initialized
+DEBUG - 2024-11-20 13:40:54 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:40:54 --> Input Class Initialized
+INFO - 2024-11-20 13:40:54 --> Language Class Initialized
+INFO - 2024-11-20 13:40:54 --> Loader Class Initialized
+INFO - 2024-11-20 13:40:54 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:40:54 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:40:54 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:40:54 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:40:54 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:40:54 --> Controller Class Initialized
+INFO - 2024-11-20 13:40:54 --> File loaded: application/views\welcome_message.php
+INFO - 2024-11-20 13:40:54 --> Final output sent to browser
+DEBUG - 2024-11-20 13:40:54 --> Total execution time: 0.0207
+INFO - 2024-11-20 13:42:23 --> Config Class Initialized
+INFO - 2024-11-20 13:42:23 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:42:23 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:42:23 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:42:23 --> URI Class Initialized
+INFO - 2024-11-20 13:42:23 --> Router Class Initialized
+INFO - 2024-11-20 13:42:23 --> Output Class Initialized
+INFO - 2024-11-20 13:42:23 --> Security Class Initialized
+DEBUG - 2024-11-20 13:42:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:42:23 --> Input Class Initialized
+INFO - 2024-11-20 13:42:23 --> Language Class Initialized
+INFO - 2024-11-20 13:42:23 --> Loader Class Initialized
+INFO - 2024-11-20 13:42:23 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:42:23 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:42:23 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:42:23 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:42:23 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:42:23 --> Controller Class Initialized
+INFO - 2024-11-20 13:42:23 --> Final output sent to browser
+DEBUG - 2024-11-20 13:42:23 --> Total execution time: 0.0204
+INFO - 2024-11-20 13:42:30 --> Config Class Initialized
+INFO - 2024-11-20 13:42:30 --> Hooks Class Initialized
+DEBUG - 2024-11-20 13:42:30 --> UTF-8 Support Enabled
+INFO - 2024-11-20 13:42:30 --> Utf8 Class Initialized
+INFO - 2024-11-20 13:42:30 --> URI Class Initialized
+INFO - 2024-11-20 13:42:30 --> Router Class Initialized
+INFO - 2024-11-20 13:42:30 --> Output Class Initialized
+INFO - 2024-11-20 13:42:30 --> Security Class Initialized
+DEBUG - 2024-11-20 13:42:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-20 13:42:30 --> Input Class Initialized
+INFO - 2024-11-20 13:42:30 --> Language Class Initialized
+INFO - 2024-11-20 13:42:30 --> Loader Class Initialized
+INFO - 2024-11-20 13:42:30 --> Helper loaded: url_helper
+INFO - 2024-11-20 13:42:30 --> Helper loaded: file_helper
+INFO - 2024-11-20 13:42:30 --> Database Driver Class Initialized
+DEBUG - 2024-11-20 13:42:30 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-20 13:42:30 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-20 13:42:30 --> Controller Class Initialized
+INFO - 2024-11-20 13:42:30 --> Final output sent to browser
+DEBUG - 2024-11-20 13:42:30 --> Total execution time: 0.0422
---
+++ application/logs/log-2024-11-21.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2024-11-21 06:48:08 --> Config Class Initialized
+INFO - 2024-11-21 06:48:08 --> Hooks Class Initialized
+DEBUG - 2024-11-21 06:48:08 --> UTF-8 Support Enabled
+INFO - 2024-11-21 06:48:08 --> Utf8 Class Initialized
+INFO - 2024-11-21 06:48:08 --> URI Class Initialized
+DEBUG - 2024-11-21 06:48:08 --> No URI present. Default controller set.
+INFO - 2024-11-21 06:48:08 --> Router Class Initialized
+INFO - 2024-11-21 06:48:08 --> Output Class Initialized
+INFO - 2024-11-21 06:48:08 --> Security Class Initialized
+DEBUG - 2024-11-21 06:48:08 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-21 06:48:08 --> Input Class Initialized
+INFO - 2024-11-21 06:48:08 --> Language Class Initialized
+INFO - 2024-11-21 06:48:08 --> Loader Class Initialized
+INFO - 2024-11-21 06:48:08 --> Helper loaded: url_helper
+INFO - 2024-11-21 06:48:08 --> Helper loaded: file_helper
+INFO - 2024-11-21 06:48:08 --> Database Driver Class Initialized
+DEBUG - 2024-11-21 06:48:08 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-21 06:48:08 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-21 06:48:08 --> Controller Class Initialized
+INFO - 2024-11-21 06:48:08 --> File loaded: application/views\welcome_message.php
+INFO - 2024-11-21 06:48:08 --> Final output sent to browser
+DEBUG - 2024-11-21 06:48:08 --> Total execution time: 0.0238
+INFO - 2024-11-21 06:51:28 --> Config Class Initialized
+INFO - 2024-11-21 06:51:28 --> Hooks Class Initialized
+DEBUG - 2024-11-21 06:51:28 --> UTF-8 Support Enabled
+INFO - 2024-11-21 06:51:28 --> Utf8 Class Initialized
+INFO - 2024-11-21 06:51:28 --> URI Class Initialized
+DEBUG - 2024-11-21 06:51:28 --> No URI present. Default controller set.
+INFO - 2024-11-21 06:51:28 --> Router Class Initialized
+INFO - 2024-11-21 06:51:28 --> Output Class Initialized
+INFO - 2024-11-21 06:51:28 --> Security Class Initialized
+DEBUG - 2024-11-21 06:51:28 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-21 06:51:28 --> Input Class Initialized
+INFO - 2024-11-21 06:51:28 --> Language Class Initialized
+INFO - 2024-11-21 06:51:28 --> Loader Class Initialized
+INFO - 2024-11-21 06:51:28 --> Helper loaded: url_helper
+INFO - 2024-11-21 06:51:28 --> Helper loaded: file_helper
+INFO - 2024-11-21 06:51:28 --> Database Driver Class Initialized
+DEBUG - 2024-11-21 06:51:28 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-21 06:51:28 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-21 06:51:28 --> Controller Class Initialized
+INFO - 2024-11-21 06:51:28 --> File loaded: application/views\welcome_message.php
+INFO - 2024-11-21 06:51:28 --> Final output sent to browser
+DEBUG - 2024-11-21 06:51:28 --> Total execution time: 0.0217
+INFO - 2024-11-21 06:51:29 --> Config Class Initialized
+INFO - 2024-11-21 06:51:29 --> Hooks Class Initialized
+DEBUG - 2024-11-21 06:51:29 --> UTF-8 Support Enabled
+INFO - 2024-11-21 06:51:29 --> Utf8 Class Initialized
+INFO - 2024-11-21 06:51:29 --> URI Class Initialized
+DEBUG - 2024-11-21 06:51:29 --> No URI present. Default controller set.
+INFO - 2024-11-21 06:51:29 --> Router Class Initialized
+INFO - 2024-11-21 06:51:29 --> Output Class Initialized
+INFO - 2024-11-21 06:51:29 --> Security Class Initialized
+DEBUG - 2024-11-21 06:51:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-11-21 06:51:29 --> Input Class Initialized
+INFO - 2024-11-21 06:51:29 --> Language Class Initialized
+INFO - 2024-11-21 06:51:29 --> Loader Class Initialized
+INFO - 2024-11-21 06:51:29 --> Helper loaded: url_helper
+INFO - 2024-11-21 06:51:29 --> Helper loaded: file_helper
+INFO - 2024-11-21 06:51:29 --> Database Driver Class Initialized
+DEBUG - 2024-11-21 06:51:29 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-11-21 06:51:29 --> Session: Class initialized using 'files' driver.
+INFO - 2024-11-21 06:51:29 --> Controller Class Initialized
+INFO - 2024-11-21 06:51:29 --> File loaded: application/views\welcome_message.php
+INFO - 2024-11-21 06:51:29 --> Final output sent to browser
+DEBUG - 2024-11-21 06:51:29 --> Total execution time: 0.0211
