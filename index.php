2c41cf1c23d439e4f3bb698fc0cd563654b90afb
alimudasirpcr <alimudasirpcr@gmail.com>
2024-11-21 09:56:25 +0400

    update

--- index.php
+++ index.php
@@ -46,6 +46,9 @@ if (defined('ENVIRONMENT'))
 }
 
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 /*
  *---------------------------------------------------------------
  * SYSTEM FOLDER NAME
